5d8952607e978b696c84e5d44de7b6522efb050f
phrompong <zphrompong.kh@pttdigital.com>
2022-04-09 22:07:50 +0700

    Update healthCheck

--- src/routes/healthCheck.route.ts
+++ src/routes/healthCheck.route.ts
@@ -5,18 +5,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  const { timeout } = _req.query;
-  let ms = 0;
-
-  // * Only allow timeout on test
-  /* istanbul ignore else */
-  if (process.env.NODE_ENV === "test") {
-    ms = +(timeout || 0);
-  }
-
-  setTimeout(() => {
-    res.status(200).respond(0, "ok", { version });
-  }, ms);
+  return res.status(200).send({ message: "ok" });
 });
 
 export default router;
