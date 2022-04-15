345e04a36131392c5f9293bdd27148e75125246e
phrompong <zphrompong.kh@pttdigital.com>
2022-04-15 23:40:57 +0700

    Add route get status

--- src/routes/v1/status.route.ts
+++ src/routes/v1/status.route.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { StatusModel } from "../../models/status.model";
 
 var router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(500).respond(99, "Not implemented yet", null);
+router.get("/", async (req, res) => {
+  try {
+    const result = await StatusModel.find().lean();
+
+    return res
+      .status(200)
+      .send({ code: "ERO-0001", message: "ok", data: result });
+  } catch (error) {
+    const err = error as Error;
+
+    return res.status(400).send({ code: "ERO-0010", message: err.message });
+  }
 });
 
 export default router;
