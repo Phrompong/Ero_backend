902507d6a3349929a0e7b57bca54eaa66d3d0e6f
phrompong <zphrompong.kh@pttdigital.com>
2022-05-08 20:05:30 +0700

    allow cors

--- src/server.ts
+++ src/server.ts
@@ -111,12 +111,7 @@ async function init() {
     .then(connectToDatabase)
     .then(() => {
       app.use(helmet());
-      app.use(
-        cors({
-          origin: "https://ero-test.asiawealth.co.th/",
-          optionsSuccessStatus: 200,
-        })
-      );
+      app.use(cors());
       app.use(expressMiddleware());
       app.use(express.text({ type: "text/plain" }));
       app.use(bodyParser.json());
