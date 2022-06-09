20344691087dca8a61028c3a0710449c43d23e7d
phrompong <zphrompong.kh@pttdigital.com>
2022-06-09 19:32:30 +0700

    Add function getTotalStockVolume

--- src/controllers/overPayment.controller.ts
+++ src/controllers/overPayment.controller.ts
@@ -1,2 +1,26 @@
+import { OrderModel } from "../models/order.model";
+
 // * Step 1 process userRight
-export async function userRight() {}
+export async function userRight() {
+  // * Sum value total stock volume
+  const totalStockVolume = await getTotalStockVolume();
+
+  const response: any = {};
+}
+
+async function getTotalStockVolume(): Promise<number> {
+  const response = await OrderModel.aggregate([
+    {
+      $group: {
+        _id: "$rightStockName",
+        totalStockVolume: {
+          $sum: "$stockVolume",
+        },
+      },
+    },
+  ]);
+
+  if (response.length === 0) return 0;
+
+  return response[0].totalStockVolume;
+}
