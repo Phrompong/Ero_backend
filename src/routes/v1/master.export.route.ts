5424cd624245f0e6e4274d8b963a7353a5f51336
phrompong <zphrompong.kh@pttdigital.com>
2022-06-14 08:43:14 +0700

    manage master export

--- src/routes/v1/master.export.route.ts
+++ src/routes/v1/master.export.route.ts
@@ -6,6 +6,28 @@ import {
 
 var router = express.Router();
 
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res
+        .status(400)
+        .send({ code: "ERO-0011", message: "id is missing" });
+    }
+
+    const result = await MasterExportModel.find().lean();
+
+    return res
+      .status(200)
+      .send({ code: "ERO-0001", message: "ok", data: result });
+  } catch (error) {
+    const err = error as Error;
+
+    return res.status(400).send({ code: "ERO-0010", message: err.message });
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
     const result = await MasterExportModel.find().lean();
@@ -33,6 +55,34 @@ router.post("/", async (req, res) => {
     const { name, value, fileExtension } = body;
 
     const result = await MasterExportModel.create({
+      name,
+      value,
+      fileExtension,
+    });
+
+    return res
+      .status(200)
+      .send({ code: "ERO-0001", message: "ok", data: result });
+  } catch (error) {
+    const err = error as Error;
+
+    return res.status(400).send({ code: "ERO-0010", message: err.message });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res
+        .status(400)
+        .send({ code: "ERO-0011", message: "id is missing" });
+    }
+
+    const { name, value, fileExtension } = req.body;
+
+    const result = await MasterExportModel.findByIdAndUpdate(id, {
       name,
       value,
       fileExtension,
